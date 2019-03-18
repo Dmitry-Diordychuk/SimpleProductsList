d0ae7f3e518655d9db1d5aad6dc0836e54ec85d6
Diord <Diord@DESKTOP-6B09NPQ>
2019-03-18 17:08:10 +0300

    Add project files.

---
+++ WorkingWithVisualStudio.Tests/Comparer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WorkingWithVisualStudio.Tests
+{
+    public class Comparer
+    {
+        public static Comparer<U> Get<U>(Func<U, U, bool> func) //Лямбда-выражение для сравнения.
+        {
+            return new Comparer<U>(func);
+        }
+    }
+
+    public class Comparer<T> : Comparer, IEqualityComparer<T>  //Для сравнения объектов, являющихся экземпляром специальных классов, понадобится метод Assert.Equal()
+    {                                                          //который принимает аргумент реализующий интерфейс IEqualityComparer<T>
+        private Func<T, T, bool> comparisonFunction;
+        public Comparer(Func<T,T,bool> func)
+        {
+            comparisonFunction = func;
+        }
+        public bool Equals(T x, T y)
+        {
+            return comparisonFunction(x, y);
+        }
+        public int GetHashCode(T obj)
+        {
+            return obj.GetHashCode();
+        }
+    }
+}
